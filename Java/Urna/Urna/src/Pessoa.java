50ad5e80c4d89f38bae0ead07731006a18b3275d
Arthur Gomes <153852894+mercuriohg@users.noreply.github.com>
2025-11-04 14:26:54 -0300

    Add files via upload

---
+++ Java/Urna/Urna/src/Pessoa.java
@@ -0,0 +1,19 @@
+public abstract class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa() {}
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() { return nome; }
+    public int getIdade() { return idade; }
+
+    public void setNome(String nome) { this.nome = nome; }
+    public void setIdade(int idade) { this.idade = idade; }
+
+    public abstract void votante();
+}
